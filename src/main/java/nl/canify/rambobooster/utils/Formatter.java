0ab04bf3863f312f73b3d0b42af9773738c6aac9
Canify <xthijsyt@gmail.com>
2022-05-17 12:58:37 +0200

    RGB support

--- src/main/java/nl/canify/rambobooster/utils/Formatter.java
+++ src/main/java/nl/canify/rambobooster/utils/Formatter.java
@@ -1,14 +1,22 @@
 package nl.canify.rambobooster.utils;
 
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Formatter {
 
+    public static final Pattern pattern = Pattern.compile("&#(\\w{5}[0-9a-f])");
     public static String color(String text) {
-        return ChatColor.translateAlternateColorCodes('&', text);
+        Matcher matcher = pattern.matcher(text);
+        StringBuffer buffer = new StringBuffer();
+        while(matcher.find()) {
+            matcher.appendReplacement(buffer, ChatColor.of("#" + matcher.group(1)).toString());
+        }
+        return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(buffer).toString());
     }
 
     public static List<String> color(List<String> text) {

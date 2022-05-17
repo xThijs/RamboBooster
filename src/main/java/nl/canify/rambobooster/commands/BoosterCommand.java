11ecbb184d4dfc793addda9cc5486e86c0553346
Canify <xthijsyt@gmail.com>
2022-05-17 12:38:22 +0200

    Small change to make console use the give booster command.

--- src/main/java/nl/canify/rambobooster/commands/BoosterCommand.java
+++ src/main/java/nl/canify/rambobooster/commands/BoosterCommand.java
@@ -7,6 +7,7 @@ import nl.canify.rambobooster.files.DataManager;
 import nl.canify.rambobooster.utils.Formatter;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandAlias("booster")
@@ -21,23 +22,23 @@ public class BoosterCommand extends BaseCommand {
     @Subcommand("give")
     @CommandPermission("rambobooster.admin")
     @CommandCompletion("@players @range:1-100")
-    public static void onGive(Player player, String[] args) {
+    public static void onGive(CommandSender sender, String[] args) {
         int amount;
         try {
             amount = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
-            player.sendMessage(Formatter.color("&cFoute syntax. Gebruik: /booster give <player> <amount>"));
+            sender.sendMessage(Formatter.color("&cFoute syntax. Gebruik: /booster give <player> <amount>"));
             return;
         }
 
         OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
         if (!target.hasPlayedBefore()) {
-            player.sendMessage(Formatter.color("&cDe speler &4" + target.getName() + " &cis nog nooit de server gejoined."));
+            sender.sendMessage(Formatter.color("&cDe speler &4" + target.getName() + " &cis nog nooit de server gejoined."));
             return;
         }
 
         DataManager.addBooster(target.getUniqueId(), amount);
-        player.sendMessage(Formatter.color("&aJe hebt &f" + amount + " &abooster(s) aan &f" + target.getName() + " &agegeven."));
+        sender.sendMessage(Formatter.color("&aJe hebt &f" + amount + " &abooster(s) aan &f" + target.getName() + " &agegeven."));
     }
 
 }
